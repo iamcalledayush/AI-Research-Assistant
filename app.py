388ace434a10084bf16fea71879e108e3a36daf7
Ayush Sharma <84268775+iamcalledayush@users.noreply.github.com>
2024-12-02 11:56:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ st.title("AI-Powered ArXiv and Document Research Assistant")
 
 st.write(
     """
-    Created by - Ayush Sharma.
+    Created by - Ayush Sharma, Uday Garg
     
     This tool allows you to input multiple arXiv links or upload multiple PDFs, and then ask questions based on the contents of those documents.
     It uses Retrieval-Augmented Generation (RAG) to retrieve relevant information from the documents and enables users to query the content and receive accurate, context-aware responses.
